ddf7446e87cd161d5d3b75e56b6981e7ab3c1422
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-05-15 16:04:05 -0500

    Se empezara a trabjar el template

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask
-
+from flask import Flask, make_response, redirect, request
 app = Flask(__name__)
 
-
 @app.route('/')
+def index():
+    user_ip = request.remote_addr 
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_id', user_ip)
+    return response
+
+@app.route('/hello')
 def hello():
-    return 'Hello World Flask'
+    user_id = request.cookies.get('user_id')
+    return f'Hola Mundo, Tu ip es {user_id}'
