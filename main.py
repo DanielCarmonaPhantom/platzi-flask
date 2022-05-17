21a73e656e056c34eb436863718774e87ed7cdf6
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-05-17 12:41:56 -0500

    Se termino el de alertas

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, make_response, redirect, request, render_template, session
+from crypt import methods
+from flask import Flask, make_response, redirect, request, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap4
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -13,6 +17,12 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Comprar Cafe', 'Enviar solicitud de compra', 'Todo M3']
 
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de Usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error = error)
@@ -25,11 +35,26 @@ def index():
     session['user_ip'] = user_ip
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
-        'todos': todos
+        'todos': todos,
+        'login_form': login_form,
+        'username': username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usuario registrado con exito')
+
+        return redirect(url_for('index'))
+
     return render_template('hello.html', **context)
